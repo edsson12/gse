f42af52f546a4693c7918d64678ef4be7d0576a9
edsson12 <edsson.cortes@gmail.com>
2023-03-11 15:53:21 -0500

    Actualizacion de logica de componente

--- src/components/CatImage/CatImage.jsx
+++ src/components/CatImage/CatImage.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import './CatImage.css'
+import axios from "axios";
 
 function CatImage() {
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
 
   const getCatImage = () => {
-    axios.get('https://api.thecatapi.com/v1/images/search')
-      .then(response => setImageUrl(response.data[0].url))
-      .catch(error => console.error(error));
+    axios
+      .get("https://api.thecatapi.com/v1/images/search")
+      .then((response) => setImageUrl(response.data[0].url))
+      .catch((error) => console.error(error));
   };
 
   return (
-    <div>
-      <button onClick={getCatImage}>Get Cat Image</button>
-      {imageUrl && <img src={imageUrl} alt="cat" />}
+    <div className="container">
+      
+      <div className="image-container">
+        {!imageUrl && <h1>Welcome, click button to get image</h1>}
+        {imageUrl && <img src={imageUrl} alt="Cat" />}
+      </div>
+      <button onClick={getCatImage}>CLICK ME!</button>
     </div>
   );
 }
