9c923376edc0ae4c01711739905c25265d066ce2
edsson12 <edsson.cortes@gmail.com>
2023-03-11 22:48:43 -0500

    enviando datos de image a slice

--- src/components/CatImage/CatImage.jsx
+++ src/components/CatImage/CatImage.jsx
@@ -1,47 +1,54 @@
 import React, { useState } from "react";
 import "./CatImage.css";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addPhoto } from "../../slice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
+//component cat image, returns a card with a random cat image
 function CatImage({ theme, toggleTheme, icon }) {
- 
   const [images, setImages] = useState([]);
-
-  const getCatImage = () => {
-    axios
+  const dispatch = useDispatch();
+  //Endpoint asynchronous call
+  async function getCatImage() {
+    await axios
       .get("https://api.thecatapi.com/v1/images/search")
       .then((response) => {
         const newImage = {
           id: response.data[0].id,
           url: response.data[0].url,
         };
+        //dispatch state of component
+        dispatch(addPhoto(newImage));
         setImages((prevState) => [...prevState, newImage]);
       })
       .catch((error) => console.error(error));
-  };
-  const removeImage = id => {
-    setImages(prevState => prevState.filter(image => image.id !== id));
+  }
+
+  const removeImage = (id) => {
+    setImages((prevState) => prevState.filter((image) => image.id !== id));
   };
 
   return (
     <div className="container" id={theme}>
-       {!images && <h1>Welcome, click button to get image</h1>} 
+      {images && <h1>Click the button and melt</h1>}
       <div className="image-container">
-      
         {images.map((image) => (
           <div className="card" key={image.id}>
             <div className="card-inside">
-            <img src={image.url} alt="cat" loading="lazy" />
-            <button className="remove" onClick={() => removeImage(image.id)}>-</button>
+              <img src={image.url} alt="cat" loading="lazy" />
+              <button className="remove" onClick={() => removeImage(image.id)}>
+                x
+              </button>
             </div>
-            <p>Hi, i´m a new image!</p>
+
+            <p>Hi, i'm a new image!</p>
           </div>
-          
         ))}
       </div>
       <button className="btn" onClick={getCatImage}>
-        CLICK ME!
+        MELT
       </button>
       <button className="toggleTheme" onClick={toggleTheme}>
         <FontAwesomeIcon
@@ -49,7 +56,6 @@ function CatImage({ theme, toggleTheme, icon }) {
           icon={icon ? faMoon : faLightbulb}
         />
       </button>
-      
     </div>
   );
 }
