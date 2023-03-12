79237522b60288d655ffaea843b602de597afa55
edsson12 <edsson.cortes@gmail.com>
2023-03-11 21:06:30 -0500

    se crea un objeto para almacenar las imagenes

--- src/components/CatImage/CatImage.jsx
+++ src/components/CatImage/CatImage.jsx
@@ -1,29 +1,55 @@
-import React, {  useState } from "react";
-import './CatImage.css'
+import React, { useState } from "react";
+import "./CatImage.css";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
-
-function CatImage({theme, toggleTheme}) {
-  const [imageUrl, setImageUrl] = useState("");
-
+function CatImage({ theme, toggleTheme, icon }) {
+ 
+  const [images, setImages] = useState([]);
 
   const getCatImage = () => {
     axios
       .get("https://api.thecatapi.com/v1/images/search")
-      .then((response) => setImageUrl(response.data[0].url))
+      .then((response) => {
+        const newImage = {
+          id: response.data[0].id,
+          url: response.data[0].url,
+        };
+        setImages((prevState) => [...prevState, newImage]);
+      })
       .catch((error) => console.error(error));
   };
-
+  const removeImage = id => {
+    setImages(prevState => prevState.filter(image => image.id !== id));
+  };
 
   return (
     <div className="container" id={theme}>
-      
+       {!images && <h1>Welcome, click button to get image</h1>} 
       <div className="image-container">
-        {!imageUrl && <h1>Welcome, click button to get image</h1>}
-        {imageUrl && <img src={imageUrl} alt="Cat" />}
+      
+        {images.map((image) => (
+          <div className="card" key={image.id}>
+            <div className="card-inside">
+            <img src={image.url} alt="cat" loading="lazy" />
+            <button className="remove" onClick={() => removeImage(image.id)}>-</button>
+            </div>
+            <p>Hi, i´m a new image!</p>
+          </div>
+          
+        ))}
       </div>
-      <button onClick={getCatImage}>CLICK ME!</button>
-      <button onClick={toggleTheme}>dm</button>
+      <button className="btn" onClick={getCatImage}>
+        CLICK ME!
+      </button>
+      <button className="toggleTheme" onClick={toggleTheme}>
+        <FontAwesomeIcon
+          color={icon ? "#092327" : "#90C2E7"}
+          icon={icon ? faMoon : faLightbulb}
+        />
+      </button>
+      
     </div>
   );
 }
