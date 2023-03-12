6c708e46c56db71db7890df8daf5562d8673642f
edsson12 <edsson.cortes@gmail.com>
2023-03-11 21:07:41 -0500

    se agrega el uso de iconos para el theme

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ export const ThemeContext = createContext(null);
 
 function App() {
   const [theme, setTheme] = useState("light");
+  const [icon,setIcon]=useState(true);
 
   const toggleTheme = () => {
     setTheme((current) => (current === "light" ? "dark" : "light"));
+    setIcon(!icon);
   };
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <CatImage theme={theme} toggleTheme={toggleTheme}/>
+      <CatImage theme={theme} toggleTheme={toggleTheme} icon={icon}/>
     </ThemeContext.Provider>
   );
 }
